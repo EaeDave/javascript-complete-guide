ca271478f3bbd57d4dcddf631894dd3b3bd214ab
EaeDave <d4vd.sv@gmail.com>
2025-09-12 10:06:00 -0300

    code: using constants

--- code/002-basics/basics-01-starting-project/assets/scripts/app.js
+++ code/002-basics/basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,11 @@
+const defaultResult = 0;
 let currentResult; // in js you would only declare without initialize a variable.
 currentResult = 0; // then, initialize a value like this.
 
 currentResult = currentResult + 10; // adding the last result plus the new result.
 currentResult += 10; // other way to do it.
 
+// defaultResult = currentResult;  // constants can't be reassign.
 let calculationDescription = '10 + 10';
 
 outputResult(currentResult, calculationDescription);
