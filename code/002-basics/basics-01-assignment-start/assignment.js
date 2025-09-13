07f100a403ce20753871b39bc9141f816ef78ba2
EaeDave <d4vd.sv@gmail.com>
2025-09-13 09:51:18 -0300

    code: practice, variables, constants, operatores and core data types

---
+++ code/002-basics/basics-01-assignment-start/assignment.js
@@ -0,0 +1,18 @@
+// Create two variables: One that holds a fictional user input (a number of your choice) and one that holds no value initially (e.g. "result").
+let userInput = 23;
+let result;
+
+// Set the result variable to 18 plus the value stored in user input.
+result = 18 + userInput;
+
+// Add three additional lines of code where you change the result variable again (by subtracting a value of your choice, multiplying it and dividing it).
+result -= 5;
+result *= 2;
+result /= 3;
+
+// Think about the value stored in the user input variable you also created - did that value change?
+// No!
+
+// alert() the result and the user input variables (in two separate alert() calls).
+alert(userInput);
+alert(result);
