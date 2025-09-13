87d7b220c175978143151ee1d083f72f615e8d52
EaeDave <d4vd.sv@gmail.com>
2025-09-13 10:09:48 -0300

    code: adding a custom function

--- code/002-basics/basics-01-starting-project/assets/scripts/app.js
+++ code/002-basics/basics-01-starting-project/assets/scripts/app.js
@@ -5,6 +5,14 @@ currentResult = 0; // then, initialize a value like this.
 currentResult = currentResult + 10; // adding the last result plus the new result.
 currentResult += 10; // other way to do it.
 
+//       add is the function's name.
+//           num1, num2 are the function's parameters.
+function add(num1, num2) {
+  // inside the braces {} there are the function's body.
+  const result = num1 + num2; // variable that only exists in the function scope.
+  alert(`The result is ${result}`);
+}
+
 // defaultResult = currentResult;  // constants can't be reassign.
 let calculationDescription = '10 + 10';
 calculationDescription = '"10 + 10"'; // showing double quotes using single quotes to define the string.
@@ -17,3 +25,4 @@ ${currentResult}`; // using backtick to interact javascript data to a string.
 let errorMessage = 'An error \n' + 'occurred!';
 
 outputResult(currentResult, errorMessage);
+add(4, 6); // calling the function using arguments 4 and 6.
