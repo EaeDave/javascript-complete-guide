c5f246498c30524b27f8908d5c89ef57f186a84c
EaeDave <d4vd.sv@gmail.com>
2025-09-14 11:48:48 -0300

    code: more about the return statement

---
+++ code/002-basics/basics-01-starting-project/assets/scripts/returnStatement.js
@@ -0,0 +1,7 @@
+function sum(num1, num2) {
+  const result = num1 + num2;
+  return result;
+  // after return statement, anything will not run.
+  // there is only possible to have one return per function.
+  console.log(result); // this will not run.
+}
