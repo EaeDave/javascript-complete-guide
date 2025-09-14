e9127d50d54aac085b296d47e6f559dced05977c
EaeDave <d4vd.sv@gmail.com>
2025-09-14 11:25:26 -0300

    code: the (un)importance of code order (hoisting)

--- code/002-basics/basics-01-starting-project/assets/scripts/app.js
+++ code/002-basics/basics-01-starting-project/assets/scripts/app.js
@@ -1,18 +1,11 @@
+let myUninitializedVariable; // it is important to declare the variable before the initialization.
+
 const defaultResult = 0;
 let currentResult; // in js you would only declare without initialize a variable.
 currentResult = 0; // then, initialize a value like this.
 
 currentResult = currentResult + 10; // adding the last result plus the new result.
 currentResult += 10; // other way to do it.
-
-//       add is the function's name.
-//           num1, num2 are the function's parameters.
-function add(num1, num2) {
-  // inside the braces {} there are the function's body.
-  const result = num1 + num2; // variable that only exists in the function scope.
-  // alert(`The result is ${result}`);
-  return result;
-}
 
 // defaultResult = currentResult;  // constants can't be reassign.
 let calculationDescription = '10 + 10';
@@ -32,3 +25,13 @@ alert(secondSum);
 // add(4, 6); // calling the function using arguments 4 and 6.
 
 outputResult(currentResult, secondSum);
+
+// we can declare a function after call it (hoisting).
+//       add is the function's name.
+//           num1, num2 are the function's parameters.
+function add(num1, num2) {
+  // inside the braces {} there are the function's body.
+  const result = num1 + num2; // variable that only exists in the function scope.
+  // alert(`The result is ${result}`);
+  return result;
+}
