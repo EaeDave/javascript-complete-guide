460f370bbdd2d5676d308a54ab965c0a00845b7c
EaeDave <d4vd.sv@gmail.com>
2025-09-16 14:29:24 -0300

    code: splitting code into functions

--- code/002-basics/basics-01-starting-project/assets/scripts/executingFunctionsIndirectly.js
+++ code/002-basics/basics-01-starting-project/assets/scripts/executingFunctionsIndirectly.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
-  currentResult = currentResult + parseInt(userInput.value); // converts text to integer.
-  outputResult(currentResult, '');
+  const enteredNumber = getUserInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber; // converts text to integer.
+  outputResult(currentResult, calcDescription);
 }
 
 // event to identify when the btn was clicked, so do something.
