135ec94a314a93082e2a6685c12ff3bac73fb7f7
EaeDave <d4vd.sv@gmail.com>
2025-09-19 10:54:45 -0300

    code: the typeof operator

---
+++ code/002-basics/basics-01-starting-project/assets/scripts/typeOfOperator.js
@@ -0,0 +1,9 @@
+let myVariable;
+console.log(typeof myVariable); // returns the type of the value (undefined in this case).
+console.log(typeof 'David'); // string.
+console.log(typeof 123); // number.
+console.log(typeof true); // boolean.
+console.log(typeof [1, 2, 3]); // object (array is an object type).
+console.log(typeof { myName: 'David', myAge: 23 }); // object.
+console.log(typeof null); // object.
+console.log(typeof NaN); // number.
